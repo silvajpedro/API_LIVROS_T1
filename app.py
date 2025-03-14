79b5f845a40d734fed004cd8bf18db1759e86a1f
silvajpedro <belojpedro.31@gmail.com>
2025-03-13 22:12:10 -0300

    banco de dados branch 'bd'

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask  # Aqui estamos importando a classe Flask do módulo flask para criar nossa aplicação
+import sqlite3  # Importamos o módulo sqlite3 para manipulação do banco de dados SQLite
 
 # Criamos uma instância do Flask e armazenamos na variável "app"
 # O parâmetro __name__ indica que este arquivo será reconhecido como o principal da aplicação
@@ -24,6 +25,29 @@ def mande_o_pix():
 def prato_do_dia():
     # Retorna uma mensagem formatada em HTML descrevendo o prato do dia
     return "<h1>O prato do dia é feijoada com farofinha com bacon e de sobremesa brownie com sorvete!!!</h1>"
+
+# Função para inicializar o banco de dados SQLite
+# Criamos uma conexão com o banco de dados chamado 'database.db'
+# Se o banco de dados ainda não existir, ele será criado automaticamente
+
+def init_db():
+    # Conectamos ao banco de dados SQLite e usamos "with" para garantir que a conexão seja fechada corretamente após a execução
+    with sqlite3.connect("database.db") as conn:
+        # Executamos um comando SQL para criar a tabela LIVROS, caso ela ainda não exista
+        conn.execute(
+            """
+                CREATE TABLE IF NOT EXISTS LIVROS(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,  # Identificador único para cada livro, gerado automaticamente
+                    titulo TEXT NOT NULL,  # O título do livro, armazenado como texto e obrigatório
+                    categoria TEXT NOT NULL,  # A categoria do livro (exemplo: ficção, tecnologia), armazenada como texto e obrigatória
+                    autor TEXT NOT NULL,  # O nome do autor do livro, armazenado como texto e obrigatório
+                    imagem_url TEXT NOT NULL  # O URL da imagem da capa do livro, armazenado como texto e obrigatório
+                )
+            """
+        )  # A execução desse comando cria a tabela caso ela ainda não exista, garantindo que nossa estrutura de banco esteja configurada
+
+# Chamamos a função para inicializar o banco de dados quando o programa for executado
+init_db()
 
 # Aqui verificamos se o script está sendo executado diretamente e não importado como módulo
 if __name__ == "__main__":
